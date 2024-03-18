1365a0a98b3ec8dcf3605270ffd304675021884f
Claire Boitet <claire.boitet@gmail.com>
2024-03-18 15:33:49 +0000

    Add test_initialise_warehouse with entry

--- test/test_warehouse.py
+++ test/test_warehouse.py
@@ -1,13 +1,19 @@
 import unittest
 from unittest import TestCase
 
-from src.warehouse import Warehouse
+from src.product import Product
+from src.warehouse import Entry, Warehouse
 
 
 class WarehouseTest(TestCase):
     def test_initialise_empty_warehouse(self):
         warehouse = Warehouse(catalogue=[])
         self.assertEqual(warehouse.catalogue, [])
+
+    def test_initialise_warehouse(self):
+        entry = Entry(Product(id=1, description='pen', price=2.5), stock=10)
+        warehouse = Warehouse(catalogue=[entry])
+        self.assertEqual(warehouse.catalogue, [entry])
 
 
 if __name__ == '__main__':
